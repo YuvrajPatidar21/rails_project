8bc5fe0538ecff001641f947913d085be08ac45a
YuvrajPatidar12 <yuvraj.patidar@codiatic.com>
2024-01-22 12:25:14 +0530

    deploying 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_controller.raise_on_missing_callback_actions = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: '0.0.0.0', port: 10000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
                                         address:         'smtp.gmail.com',
