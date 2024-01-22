c47f2d432738a0790dd1a5563e59466cd5277068
YuvrajPatidar12 <yuvraj.patidar@codiatic.com>
2024-01-22 12:49:24 +0530

    deploying 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -94,23 +94,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-
-
-  config.action_mailer.perform_deliveries = true
-  config.action_controller.raise_on_missing_callback_actions = true
-  config.action_mailer.default_url_options = { host: '0.0.0.0', port: 10000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-                                        address:         'smtp.gmail.com',
-                                        port:            587,
-                                        domain:          'example.com',
-                                        user_name:       ENV['SMTP_USERNAME'],
-                                        password:        ENV['SMTP_PASSWORD'],
-                                        authentication:  'plain',
-                                        enable_starttls: true,
-                                        open_timeout:    5,
-                                        read_timeout:    5 
-                                      }
-
+  config.action_mailer.delivery_method = :letter_opener
 
 end
