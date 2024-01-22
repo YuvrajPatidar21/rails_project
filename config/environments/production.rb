7a6bd325d59955f7404287becc6243b2d13209ec
YuvrajPatidar12 <yuvraj.patidar@codiatic.com>
2024-01-22 13:06:42 +0530

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.action_mailer.delivery_method = :letter_opener
 
 end
