75f93716cf77ed2be3ffabd16ed4f75286eb2e63
YuvrajPatidar12 <yuvraj.patidar@codiatic.com>
2024-01-22 11:42:10 +0530

    deploying

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+admin = User.create(email:"admin@123gmail.com", name: "Admin", mobile: 9993999123, address: "Indore", city: "Indore", date_of_birth: "1999-03-10", status: "Single", role: "admin", state: "Madhy
+a Pradesh", zipcode: 458110, password: "admin@123")
